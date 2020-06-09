d694dc94d82aa960b98882ba3e1e05e8234b5108
张珊 <1173236712@qq.com>
2020-06-09 17:05:44 +0800

    部署

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 // 导入组件
 import Home from './pages/Home'
 import CityList from './pages/CityList'
