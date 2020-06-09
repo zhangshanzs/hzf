522a8515b1eb552592f81b4f8667c495e0e08093
张珊 <1173236712@qq.com>
2020-06-09 16:43:59 +0800

    Updates

---
+++ precache-manifest.c245d26fcd9f3e993e6030622375a9bb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b800fe0609e58d579a9480c2b27bb78",
+    "url": "/hzf/index.html"
+  },
+  {
+    "revision": "431806d395efbc809a63",
+    "url": "/hzf/static/css/2.90071de8.chunk.css"
+  },
+  {
+    "revision": "2b135906b27e1fe1c829",
+    "url": "/hzf/static/css/main.98f38c16.chunk.css"
+  },
+  {
+    "revision": "431806d395efbc809a63",
+    "url": "/hzf/static/js/2.255674d1.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/hzf/static/js/2.255674d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b135906b27e1fe1c829",
+    "url": "/hzf/static/js/main.f426a824.chunk.js"
+  },
+  {
+    "revision": "1a0b7a6b72205d6450a6",
+    "url": "/hzf/static/js/runtime-main.5de691f3.js"
+  },
+  {
+    "revision": "3b538f76f922de087df0eb82f703119a",
+    "url": "/hzf/static/media/iconfont.3b538f76.ttf"
+  },
+  {
+    "revision": "80483e3b898177297ad8b08c1e002f55",
+    "url": "/hzf/static/media/iconfont.80483e3b.eot"
+  },
+  {
+    "revision": "c931e1ae5f45a521413c9905641f2071",
+    "url": "/hzf/static/media/iconfont.c931e1ae.svg"
+  },
+  {
+    "revision": "f58b610e9d4e6470d644d7d195bbfda6",
+    "url": "/hzf/static/media/iconfont.f58b610e.woff"
+  }
+]);
